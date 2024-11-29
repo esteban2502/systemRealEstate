dfe033293248cf62fba340b5bb2ad62e1f862a50
Juan Esteban <juanes9001@gmail.com>
2024-11-29 10:29:19 -0500

    service

---
+++ src/main/java/com/systemRealEstate/systemRealEstate/service/IPropertyService.java
@@ -0,0 +1,20 @@
+package com.systemRealEstate.systemRealEstate.service;
+
+
+import com.systemRealEstate.systemRealEstate.model.Property;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface IPropertyService {
+
+ List<Property> getAll();
+ Optional<Property> getById(Long id);
+ void addPropery(Property property);
+ void updateProperty(Long id, Property updatedProperty);
+ void deleteById(Long id);
+
+
+}
---
+++ src/main/java/com/systemRealEstate/systemRealEstate/service/Implementation/PropertyServiceImpl.java
@@ -0,0 +1,52 @@
+package com.systemRealEstate.systemRealEstate.service.Implementation;
+
+import com.systemRealEstate.systemRealEstate.model.Property;
+import com.systemRealEstate.systemRealEstate.repository.PropertyRepository;
+import com.systemRealEstate.systemRealEstate.service.IPropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class PropertyServiceImpl implements IPropertyService {
+
+
+    @Autowired
+    private PropertyRepository repository;
+
+    @Override
+    public List<Property> getAll() {
+        Iterable<Property> properties = repository.findAll();
+        List<Property> list = new ArrayList<>();
+        for(Property aux : properties){
+            list.add(aux);
+        }
+        return list;
+    }
+
+    @Override
+    public Optional<Property> getById(Long id) {
+    Optional<Property> property = repository.findById(id);
+    if(property.isPresent()){
+            return property;
+        }
+        return null;
+    }
+
+    @Override
+    public void addPropery(Property property) {
+        repository.save(property);
+    }
+
+    @Override
+    public void updateProperty(Long id, Property updatedProperty) {
+        updatedProperty.setId(id);
+        repository.save(updatedProperty);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+}
