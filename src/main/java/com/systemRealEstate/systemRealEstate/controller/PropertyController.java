1f848df6b6052522908189894ea9cc57df1085d5
Juan Esteban <juanes9001@gmail.com>
2024-11-29 11:23:01 -0500

    agregar

--- src/main/java/com/systemRealEstate/systemRealEstate/controller/PropertyController.java
+++ src/main/java/com/systemRealEstate/systemRealEstate/controller/PropertyController.java
@@ -3,12 +3,12 @@ package com.systemRealEstate.systemRealEstate.controller;
 import com.systemRealEstate.systemRealEstate.controller.DTO.PropertyDTO;
 import com.systemRealEstate.systemRealEstate.model.Property;
 import com.systemRealEstate.systemRealEstate.service.IPropertyService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,5 +35,19 @@ public class PropertyController {
     return ResponseEntity.ok(listDTO);
 
 
+    }
+
+
+    @PostMapping("/property")
+    public ResponseEntity<?> addProperty(@RequestBody PropertyDTO propertyDTO){
+        Property property = new Property(
+                propertyDTO.getId(), propertyDTO.getName(), propertyDTO.getName(),
+                propertyDTO.getPrice(),propertyDTO.getImages(),propertyDTO.getLatitude(),
+                propertyDTO.getLength()
+        );
+
+        service.addPropery(property);
+
+        return ResponseEntity.created(URI.create("/api/property"+property.getId())).build();
     }
 }
